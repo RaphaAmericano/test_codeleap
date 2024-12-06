fe626f0e5b2543fe5f45e31bc7dbb63f99f24b46
Raphael Americano <raphael@raphaelamericano.com.br>
2024-12-06 12:42:18 -0300

    change response function

--- api/views.py
+++ api/views.py
@@ -4,22 +4,29 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from api.serializers import PostSerializer
 from base.models import Post
-
+from django.http import JsonResponse
 
 @api_view(['GET', 'POST'])
 def careers(request):
     if request.method == 'GET':
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+        # return Response(serializer.data)
+        return JsonResponse(serializer.data, safe=False)
 
     elif request.method == 'POST':
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-        return Response(serializer.data)
+            return JsonResponse({ 
+                'username': serializer.data['username'],
+                'title': serializer.data['title'],
+                'content': serializer.data['content'],
+                }, status=status.HTTP_201_CREATED)
+        return JsonResponse({ 'error': 'Invalid data', 'details': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
     else:
-        return Response('Method not allowed')
+        return JsonResponse({ 'error': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 @api_view(['PATCH','DELETE'])
 def careers_param_request(request, id):
@@ -33,19 +40,19 @@ def careers_param_request(request, id):
                     'content': serializer.data['content'],
                     'title': serializer.data['title']
                 }
-                return Response(response_data, status=status.HTTP_200_OK)
+                return JsonResponse(response_data, status=status.HTTP_200_OK)
             
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Post.DoesNotExist:
-            return Response('Post not found', status=status.HTTP_404_NOT_FOUND)
+            return JsonResponse({ 'error': 'Post not found' }, status=status.HTTP_404_NOT_FOUND)
         
     elif request.method == 'DELETE':
         try: 
             post = Post.objects.get(id=id)
             post.delete()
-            return Response({}, status=status.HTTP_204_NO_CONTENT)
+            return JsonResponse({}, status=status.HTTP_204_NO_CONTENT)
         except Post.DoesNotExist:
-            return Response('Post not found', status=status.HTTP_404_NOT_FOUND)
+            return JsonResponse({ 'error':'Post not found'}, status=status.HTTP_404_NOT_FOUND)
         
     else:
-        return Response('Method not allowed')
+        return JsonResponse({ 'error': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
--- test_codeleap/settings.py
+++ test_codeleap/settings.py
@@ -98,6 +98,23 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    },
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
